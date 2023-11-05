16c0c6dc7ad932140bc2f5d6e88e0e4c581c707d
vandodev <vandoaparecido@hotmail.com>
2023-11-05 08:16:31 -0300

    :hammer: :wrench: :construction_worker: Seletor before

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
-     <h2>Hello Tailwind</h2>
+     <h2 className="font-bold flex items-center gap-3 text-5xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h2>
      <p>Hello Tailwind</p>
     </div>
   )
