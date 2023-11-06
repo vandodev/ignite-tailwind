3dff2ab7f374645d954f97d20428559455db92eb
vandodev <vandoaparecido@hotmail.com>
2023-11-06 07:59:32 -0300

    :hammer: :wrench: :construction_worker: Responsividade e Breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-     <h2 className="font-bold flex items-center gap-3 text-5xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h2>
+     <h2 className="font-bold flex items-center gap-3 text-3xl sm:text-5xl lg:text-6xl before:w-0.5 before:h-8 before:bg-sky-500 before:flex">Hello Tailwind</h2>
      <p>Hello Tailwind</p>
      <button disabled className="bg-sky-500 px-4 py-2 rounded-md font-medium mt-4 enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">
         Sign In
