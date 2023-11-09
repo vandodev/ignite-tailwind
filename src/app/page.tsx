5ad2f425852ba0c62d9c21bce755e0101fbf6eaf
vandodev <vandoaparecido@hotmail.com>
2023-11-09 07:16:50 -0300

    :hammer: :wrench: :construction_worker: Criando Abas com Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image'
+import { SettingsTabs } from '@/components/SettingsTabs'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1 className="to-zinc-900 text-3xl font-medium">Settings</h1>
+
+      <SettingsTabs />
+    </>
   )
 }
