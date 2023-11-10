2f5296f5c39c21014290575f9953a42b24efdc31
vandodev <vandoaparecido@hotmail.com>
2023-11-10 08:41:23 -0300

    :hammer: :wrench: :construction_worker: Upload do Avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
       maxWidth:{
         app: "700px"
       },
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
      
     },
   },
