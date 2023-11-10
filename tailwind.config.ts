85e0d4dd68edcc4ad74323f82fb31ff8554a3b6b
vandodev <vandoaparecido@hotmail.com>
2023-11-10 08:24:23 -0300

    :hammer: :wrench: :construction_worker: Campos do Formulário

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,12 @@ const config: Config = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
       maxWidth:{
         app: "700px"
       },
-      colors: {
-        // Evandro: '#333'
-      }
+     
     },
   },
   plugins: [],
