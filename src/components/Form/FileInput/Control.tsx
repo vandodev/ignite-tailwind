22a3d4e72d3412564cdc71e9d52417a68d7592b2
vandodev <vandoaparecido@hotmail.com>
2023-11-11 07:12:05 -0300

    :hammer: :wrench: :construction_worker: Gerando id com o useId do react

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,7 +1,10 @@
+'use client'
 import { ComponentProps } from 'react'
+import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
-  return <input type="file" className="sr-only" id="photo" {...props} />
+  const { id} = useFileInput()
+  return <input type="file" className="sr-only" id={id} {...props} />
 }
--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,21 @@
-import { ComponentProps } from 'react'
+'use client'
+import { ComponentProps,createContext, useContext,useId } from 'react'
 
 export type RootProps = ComponentProps<'div'>
 
-export function Root(props: RootProps) {
-  return <div {...props} />
+type FileInputContextType = {
+  id: string
 }
+
+const FileInputContext = createContext({} as FileInputContextType)
+
+export function Root(props: RootProps) {
+  const id = useId()
+  return (
+    <FileInputContext.Provider value={{ id,}}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
