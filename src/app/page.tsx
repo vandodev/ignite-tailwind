d5be8d0a83e31028ca12023926af0e8a97a156a1
vandodev <vandoaparecido@hotmail.com>
2023-11-12 07:46:47 -0300

    :hammer: :wrench: :construction_worker: Criando Select de Pais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 import * as Input from '@/components/Input'
 import { Mail } from 'lucide-react'
 import * as FileInput from '@/components/Form/FileInput'
+import { Select } from '@/components/Form/Select'
 
 export default function Home() {
   return (
@@ -113,7 +114,7 @@ export default function Home() {
             >
               Country
             </label>
-            <div />
+            <Select />
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
