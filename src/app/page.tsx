b8f0d62c2de3a475c7d8992d937ec2be62d61423
vandodev <vandoaparecido@hotmail.com>
2023-11-14 07:31:58 -0300

    :hammer: :wrench: :construction_worker: Variantes de Botões

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import * as FileInput from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { Textarea } from '@/components/Form/Textarea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -23,19 +24,19 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
+            <Button
+              variant="outline" 
               type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
             >
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
               type="submit"
               form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
             >
               Save
-            </button>
+            </Button>
           </div>
         </div>
 
@@ -228,18 +229,19 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
+          <Button
+              variant="outline" 
               type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
             >
               Cancel
-            </button>
-            <button
+            </Button>
+            <Button
+              variant="primary"
               type="submit"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              form="settings"
             >
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
