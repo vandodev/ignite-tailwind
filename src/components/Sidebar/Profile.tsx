c9c66ee40a6bd92b938fcad10ccfaa8064e3f8f4
vandodev <vandoaparecido@hotmail.com>
2023-11-16 09:01:24 -0300

    :hammer: :wrench: :construction_worker: Tema dark da sidebar

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -10,10 +10,10 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+      <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Evandro Oliveira
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           vandoaparecido@hotmail.com
         </span>
       </div>
