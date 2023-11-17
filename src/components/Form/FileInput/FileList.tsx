45b1ebfad2ca36c5490b0b4026fb6dab0be54ec4
vandodev <vandoaparecido@hotmail.com>
2023-11-17 07:20:06 -0300

    :hammer: :wrench: :construction_worker: Tema dark do formulário

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -11,7 +11,7 @@ export function FileList() {
   return (
     <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
-        return <FileItem key={file.name} name={file.name} size={file.size} state='progress'  />
+        return <FileItem key={file.name} name={file.name} size={file.size} state='complete'  />
       })}
     </div>
   )
